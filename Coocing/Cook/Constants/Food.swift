c87f36a4368df5d8574d9a33492a008c5c48ea8a
3Vvvprog <333vovk@mail.ru>
2023-10-16 15:04:36 +0300

    Незначительные изменения

--- Coocing/Cook/Constants/Food.swift
+++ Coocing/Cook/Constants/Food.swift
@@ -12,8 +12,8 @@ struct Food {
     static var breakfastItems: [FoodItem] = [
         FoodItem(name: "Брауни", image: UIImage(named: "брауни")!, numberOfIngredients: "6 ингридиентов", nameOfPortions: "6 порций", time: "40 минут"),
         FoodItem(name: "Сырники из творога", image: UIImage(named: "сырники")!, numberOfIngredients: "5 ингридиентов", nameOfPortions: "2 порции", time: "30 минут"),
-        FoodItem(name: "Брауни", image: UIImage(named: "брауни")!, numberOfIngredients: "6 ингридиентов", nameOfPortions: "6 порций", time: "40 минут"),
-        FoodItem(name: "Брауни", image: UIImage(named: "брауни")!, numberOfIngredients: "6 ингридиентов", nameOfPortions: "6 порций", time: "40 минут"),
+        FoodItem(name: "Яйца-крэмбл", image: UIImage(named: "Яйца-крэмбл")!, numberOfIngredients: "6 ингридиентов", nameOfPortions: "2 порции", time: "5 минут"),
+        FoodItem(name: "Завтрак приморский", image: UIImage(named: "завтракПриморский")!, numberOfIngredients: "11 ингридиентов", nameOfPortions: "1 порция", time: "30 минут"),
     ]
     
     static var snacksItems: [FoodItem] = [
